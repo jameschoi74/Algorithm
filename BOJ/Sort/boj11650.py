7bdca4ae67a129c9e30d8729aff67ad1b5730cc2
jameschoi74 <mannerplay24@gmail.com>
2026-03-19 17:24:19 +0900

    BOJ11650 : 좌표 정렬하기

---
+++ BOJ/Sort/boj11650.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+board = []
+for _ in range(n):
+    board.append(list(map(int, sys.stdin.readline().split())))
+
+board.sort(key=lambda x: (x[0], x[1]))
+
+for each in board:
+    print(each[0], each[1])
