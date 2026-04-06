bec7ad6bc57d24488d63a463c290807c4a70e86c
jameschoi74 <mannerplay24@gmail.com>
2026-04-06 17:23:51 +0900

    BOJ4948 : 베르트랑 공준

---
+++ BOJ/15_약수, 배수와 소수 2/boj4948.py
@@ -0,0 +1,23 @@
+import sys
+
+isPrime = [1 for _ in range(123456*2 + 1)]
+isPrime[1] = 0
+
+for i in range(2, int((123456*2) ** 0.5)+1):
+    if isPrime[i] == 0:
+        continue
+
+    for j in range(i * i, 123456 * 2 + 1, i):
+        isPrime[j] = 0
+
+while True:
+    n = int(sys.stdin.readline())g
+
+    if n == 0:
+        break
+
+    cnt = 0
+    for i in range(n+1, 2*n + 1):
+        if isPrime[i]:
+            cnt += 1
+    print(cnt)
