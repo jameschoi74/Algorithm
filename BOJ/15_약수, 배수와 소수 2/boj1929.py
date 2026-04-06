02e600b055898f724d1d2154f2e1870eafe28b2b
jameschoi74 <mannerplay24@gmail.com>
2026-04-06 17:00:59 +0900

    BOJ1929 : 소수 구하기

--- BOJ/15_약수, 배수와 소수 2/boj1929.py
+++ BOJ/15_약수, 배수와 소수 2/boj1929.py
@@ -1,16 +1,19 @@
+# 에라토스테네스의 체
+
 import sys
 
 m, n = list(map(int, sys.stdin.readline().split()))
 
-def is_prime(x):
-    if x < 2:
-        return 0
+isPrime = [1 for _ in range(n+1)]
+isPrime[1] = 0
 
-    for i in range(2, int(x**0.5) + 1):
-        if x % i == 0:
-            return 0
-    return 1
+for i in range(2, int(n**0.5)+1):
+    if isPrime[i] == 0:
+        continue
+
+    for j in range(i*i, n+1, i): # i*2, i*3 이런 건 앞에서 이미 지워짐!!
+        isPrime[j] = 0
 
 for i in range(m, n+1):
-    if is_prime(i):
+    if isPrime[i]:
         print(i)
